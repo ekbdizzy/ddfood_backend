ac0dc476bb88728f51b0cbb1d3c056783f26120b
Aleksey <ekbdizzy@yandex.ru>
2020-02-28 14:38:25 +0300

    add static_root

--- ddfood_backend/settings.py
+++ ddfood_backend/settings.py
@@ -104,4 +104,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_UR = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
